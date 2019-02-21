8cc66116e26eb0906cd484cd57543b69f8d724fd
gl99 <43945605+gl99@users.noreply.github.com>
2019-02-21 17:01:06 -0500

    Add files via upload

--- web clock/script.js
+++ web clock/script.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
-    // your code goes in here
+	var circle1time= 1000;
+   setInterval(function() {
+   	$(".circle1").each(function(){
+		var pos= Math.random()*90+"vw";
+	    $(this).animate({"left" : pos, "top" : 0}, circle1time, "linear", function(){
+	    	var pos2= Math.random()*90+"vh";
+	    	$(this).animate({"left" : "90vw", "top" : pos2}, circle1time, "linear", function(){
+	    		var pos3= Math.random()*90+"vw";
+	    		$(this).animate({"left" : pos3, "top" : "90vh"}, circle1time, "linear", function(){
+	    			var pos4= Math.random()*90+"vh";
+	    			$(this).animate({"left" : 0, "top" : pos4},  circle1time, "linear");
+	    		});
+	    	});
+	    });
+   	});
+   
+   }, circle1time*4);
 
+
+
+   var circle3time= 1000;
+   setInterval(function() {
+   	$(".circle3").each(function(){
+		var pos= Math.random()*90+"vw";
+	    $(this).animate({"left" : pos, "top" : 0}, circle3time, function(){
+	    	var pos2= Math.random()*90+"vh";
+	    	$(this).animate({"left" : "90vw", "top" : pos2}, circle3time, function(){
+	    		var pos3= Math.random()*90+"vw";
+	    		$(this).animate({"left" : pos3, "top" : "90vh"}, circle3time, function(){
+	    			var pos4= Math.random()*90+"vh";
+	    			$(this).animate({"left" : 0, "top" : pos4}, circle3time);
+	    		});
+	    	});
+	    });
+   	});
+   
+   }, circle3time*4);
     
 });
