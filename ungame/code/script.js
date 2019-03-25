67ffb90b14ecedb9acab8a84a2e04e7e4881060f
gl99 <43945605+gl99@users.noreply.github.com>
2019-03-24 21:09:15 -0400

    Add files via upload

--- ungame/code/script.js
+++ ungame/code/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({makeResponseive: true, autoPlay: false});
+  mugen.initialize({attackRadius: 100, monsterHitRadius: 150, attackDuration:600, jumpHeight: 300, makeResponsive:true, autoPlay:false});
 });
+
