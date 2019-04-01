d64d16419f70b8621855ba556d2e7100618cf008
gl99 <43945605+gl99@users.noreply.github.com>
2019-04-01 17:34:18 -0400

    Delete script.js

--- BoatSlayer/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var mugen = new MUGEN();
-  mugen.initialize({ titleScreenImageURL: "./img/landscape.png", attackRadius: 85, monsterHitRadius: 150, attackDuration:600, jumpHeight: 275, makeResponsive:true, autoPlay:false});
-});
-
