9a63652028dcaccd048d19f75871370f05c8acc0
gl99 <43945605+gl99@users.noreply.github.com>
2019-04-04 13:07:07 -0400

    Add files via upload

--- boatslayer/script.js
+++ boatslayer/script.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+	var playaudio=function(){
+		document.querySelector("audio").play();
+		console.log("playaudio")
+	};
   var mugen = new MUGEN();
-  mugen.initialize({ titleScreenImageURL: "./img/landscape.png", attackRadius: 85, monsterHitRadius: 150, attackDuration:600, jumpHeight: 275, makeResponsive:true, autoPlay:false});
+  mugen.initialize({ titleScreenImageURL: "./img/landscape.png", attackRadius: 85, monsterHitRadius: 150, attackDuration:600, jumpHeight: 275, makeResponsive:true, autoPlay:false, onGameStart: playaudio });
 });
 
